148b4c88ec90b45e56c25e2537f8ff7a557f00b1
ayoub <Elma0076@algonquinlive.com>
2024-02-15 16:14:30 -0500

    step3: finished call and camera button are working perfectly

--- app/src/main/java/algonquin/cst2335/elma0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/elma0076/MainActivity.java
@@ -5,70 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     private static String TAG = "MainActivity";
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w( "MainActivity", "The application is now visible on screen" );
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w( "MainActivity", "The application is now responding to user input" );
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w( "MainActivity", "The application no longer responds to user input" );
-
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w( "MainActivity", "The application is no longer visible" );
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w( "MainActivity", "Any memory used by the application is freed" );
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
-        //Log.d(TAG, "Message");
 
-        Button loginBotton = findViewById(R.id.loginBtn);
-        loginBotton.setOnClickListener(clk->{sendMessage(clk);});
+        Log.w( TAG, "In onCreate() - Loading Widgets" );
+        Log.w(TAG, "Inside onCreate() - This is called when the activity is first created.");
 
+        Button loginButton;
+        loginButton = findViewById(R.id.login_btn);
+
+        TextView EmailEditTxt;
+        EmailEditTxt = findViewById(R.id.email_editTxt);
+
+        loginButton.setOnClickListener(click ->{
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            nextPage.putExtra("EmailAddress", EmailEditTxt.getText().toString());
+            startActivity(nextPage);
+        });
     }
-    public void sendMessage(View view){
-        Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        //String message = editText.getText().toString();
-        //nextPage.putExtra(EXTRA_MESSAGE, message);
 
-        TextView email= findViewById(R.id.EmailAddress) ;
-        nextPage.putExtra("EmailAddress",email.getText().toString());
-        startActivity(nextPage);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        super.onStart();
+        Log.w(TAG, "Inside onStart() - This is called when the activity becomes visible to the user.");
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "Inside onStop() - This is called when the activity is no longer visible to the user.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "Inside onPause() - The paused activity is called when the current activity is being paused and the previous activity is being resumed.");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG, "Inside onResume() - This is called when the user starts interacting with the application.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "Inside onDestroy() - This is called before the activity is destroyed by the system.");
+    }
 }
--- app/src/main/java/algonquin/cst2335/elma0076/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/elma0076/SecondActivity.java
@@ -9,62 +9,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
-        TextView welcomeMessage = findViewById(R.id.textViewTop);
-        welcomeMessage.setText("Welcome back "+ emailAddress);
+
+        TextView welcomeBackMsg;
+        welcomeBackMsg = findViewById(R.id.welcome_txtView);
+        if (emailAddress != null && !emailAddress.isEmpty()) {
+            welcomeBackMsg.setText("Welcome Back, " + emailAddress);
+        } else {
+            welcomeBackMsg.setText("Welcome Back");
+        }
 
 
+        EditText phoneNumber;
+        phoneNumber = findViewById(R.id.editTextPhone);
 
-        Button callBtn =  findViewById(R.id.callBtn);
-        callBtn.setOnClickListener(clk-> {
-            TextView number = findViewById(R.id.phoneNmber);
+        Button callNumber;
+        callNumber = findViewById(R.id.callNumber_btn);
+
+        callNumber.setOnClickListener( click->{
             Intent call = new Intent(Intent.ACTION_DIAL);
-            call.setData(Uri.parse("tel:" + number));
+            call.setData(Uri.parse("tel:" + phoneNumber.getText().toString()));
             startActivity(call);
         });
 
-        Button changeBtn = findViewById(R.id.changeBtn);
-        changeBtn.setOnClickListener(clk-> {
-            ImageView phoneImg = findViewById(R.id.phoneImg);
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ImageView profileImage;
+        profileImage = findViewById(R.id.picture);
 
-            ActivityResultLauncher <Intent> cameraResult = registerForActivityResult( new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        Button changeImg;
+        changeImg = findViewById(R.id.changePic_btn);
 
-                @Override
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                public void onActivityResult(ActivityResult result) {
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
 
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        Bitmap thumbnail = data.getParcelableExtra("data");
-                        phoneImg.setImageBitmap( thumbnail);
+                        if (result.getResultCode() == Activity.RESULT_OK){
 
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            profileImage.setImageBitmap(thumbnail);
+
+                        }
 
                     }
-
                 }
+        );
 
-            });
+        changeImg.setOnClickListener( click->{
             cameraResult.launch(cameraIntent);
-
         });
-
-
     }
-
-
 }
